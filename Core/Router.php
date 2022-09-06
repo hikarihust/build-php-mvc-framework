43085bdd1dcd9522370792a6b555129c59c086f4
vudinhquang <vudinhquangk53@gmail.com>
2022-09-07 00:45:01 +0900

    Get the controller and action from a URL with a variable structure

--- Core/Router.php
+++ Core/Router.php
@@ -26,8 +26,17 @@ class Router
      *
      * @return void
      */
-    public function add($route, $params)
+    public function add($route, $params = [])
     {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^' . $route . '$/i';
+
         $this->routes[$route] = $params;
     }
 
@@ -51,21 +60,18 @@ class Router
      */
     public function match($url)
     {
-        // Match to the fixed URL format /controller/action
-        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+        foreach ($this->routes as $route => $params) {
+            if (preg_match($route, $url, $matches)) {
 
-        if (preg_match($reg_exp, $url, $matches)) {
-            // Get named capture group values
-            $params = [];
-
-            foreach ($matches as $key => $match) {
-                if (is_string($key)) {
-                    $params[$key] = $match;
+                foreach ($matches as $key => $match) {
+                    if (is_string($key)) {
+                        $params[$key] = $match;
+                    }
                 }
-            }
 
-            $this->params = $params;
-            return true;
+                $this->params = $params;
+                return true;
+            }
         }
 
         return false;
--- public/index.php
+++ public/index.php
@@ -14,7 +14,13 @@ $router = new Router();
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
 $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
-$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+$router->add('{controller}/{action}');
+$router->add('admin/{controller}/{action}');
+
+// Display the routing table
+echo '<pre>';
+echo htmlspecialchars(print_r($router->getRoutes(), true));
+echo '</pre>';
 
 // Match the requested route
 $url = $_SERVER['QUERY_STRING'];
