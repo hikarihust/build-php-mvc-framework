6f624c51ef6b85bcce0496a8fa68f8d49c551f32
vudinhquang <vudinhquangk53@gmail.com>
2022-09-09 01:26:46 +0900

    Dispatch the route: create the controller object and run the action method

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@
  * Front controller
  */
 
+// Require the controller class
+require '../App/Controllers/Posts.php';
+
 /**
  * Routing
  */
@@ -13,22 +16,7 @@ $router = new Router();
 
 // Add the routes
 $router->add('', ['controller' => 'Home', 'action' => 'index']);
-$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
 $router->add('{controller}/{action}');
 $router->add('{controller}/{id:\d+}/{action}');
 
-// Display the routing table
-echo '<pre>';
-echo htmlspecialchars(print_r($router->getRoutes(), true));
-echo '</pre>';
-
-// Match the requested route
-$url = $_SERVER['QUERY_STRING'];
-
-if ($router->match($url)) {
-    echo '<pre>';
-    var_dump($router->getParams());
-    echo '</pre>';
-} else {
-    echo "No route found for URL '$url'";
-}
+$router->dispatch($_SERVER['QUERY_STRING']);
