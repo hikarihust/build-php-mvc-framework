6038b69a78e1d30a34d325d048e5800583e14c0a
vudinhquang <vudinhquangk53@gmail.com>
2022-09-12 00:49:00 +0900

    Action filters: call a method before and after every action in a controller

--- App/Controllers/Posts.php
+++ App/Controllers/Posts.php
@@ -14,11 +14,9 @@ class Posts extends \Core\Controller
      *
      * @return void
      */
-    public function index()
+    public function indexAction()
     {
         echo 'Hello from the index action in the Posts controller!';
-        echo '<p>Query string parameters: <pre>' .
-            htmlspecialchars(print_r($_GET, true)) . '</pre></p>';
     }
 
     /**
@@ -26,7 +24,7 @@ class Posts extends \Core\Controller
      *
      * @return void
      */
-    public function addNew()
+    public function addNewAction()
     {
         echo 'Hello from the addNew action in the Posts controller!';
     }
@@ -36,7 +34,7 @@ class Posts extends \Core\Controller
      *
      * @return void
      */
-    public function edit()
+    public function editAction()
     {
         echo 'Hello from the edit action in the Posts controller!';
         echo '<p>Route parameters: <pre>' .
